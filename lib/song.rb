9c9815ba5cd8aa669ba457e56abce21c96bb1621
Ashley Bland <ashleyjbland@gmail.com>
2018-09-16 20:27:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,7 @@ class Song
 
   def self.new_from_filename(filename)
     new_song = self.new
-    new_song.name = filename.split(" - ")[1].delete("mp3")
+    new_song.name = filename.split(" - ")[1].delete("mp3", "mp3")
     new_song.artist_name = filename.split(" - ")[0]
     new_song
   end
